e9f8a92528c67a2edb6e5ee36b9dedb5fd3323a0
jenswoltering <info@je-wo.com>
2017-08-02 18:42:57 +0200

    added postcss to gulp pipe

--- theme-adrett/gulpfile.js
+++ theme-adrett/gulpfile.js
@@ -1,6 +1,17 @@
-var gulp       = require('gulp'),
-    less       = require('gulp-less'),
-    rename     = require('gulp-rename');
+var gulp       = require('gulp');
+var less       = require('gulp-less');
+var rename     = require('gulp-rename');
+var postcss    = require('gulp-postcss');
+var flexbugsfixes = require('postcss-flexbugs-fixes');
+var autoprefixer = require('autoprefixer');
+
+var processors = [
+    flexbugsfixes,
+    autoprefixer({
+        browsers: ['last 2 versions', '> 0.1%']
+    })
+];
+
 
 gulp.task('default', function () {
     return gulp.src('less/theme.less', {base: __dirname})
@@ -9,6 +20,7 @@ gulp.task('default', function () {
             // the compiled file should be stored in the css/ folder instead of the less/ folder
             file.dirname = file.dirname.replace('less', 'css');
         }))
+        .pipe(postcss(processors))
         .pipe(gulp.dest(__dirname));
 });
 
